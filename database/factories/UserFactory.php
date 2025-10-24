e89b906f368b869e550ea1ffe942845310af4742
tafifa <tafifa09@gmail.com>
2025-10-24 07:35:47 +0700

    feat(factory): Update UserFactory to use Cloudinary for user avatar images

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,12 +21,14 @@ class UserFactory extends Factory
     {
         $foodTypeOptions = ['Nusantara', 'Internasional', 'Seafood', 'Kafein', 'Non-Kafein', 'Vegetarian', 'Dessert', 'Makanan Ringan', 'Pastry'];
         $placeValueOptions = ['Harga Terjangkau', 'Rasa Autentik', 'Menu Bervariasi', 'Buka 24 Jam', 'Jaringan Lancar', 'Estetika', 'Suasana Tenang', 'Suasana Tradisional', 'Suasana Homey', 'Pet Friendly', 'Ramah Keluarga', 'Pelayanan Ramah', 'Cocok untuk Nongkrong', 'Cocok untuk Work From Cafe', 'Tempat Bersejarah'];
+        $userAvatarOptions = ['avatar_m1_hdpi.png', 'avatar_m2_hdpi.png', 'avatar_m3_hdpi.png', 'avatar_m4_hdpi.png', 'avatar_f1_hdpi.png', 'avatar_f2_hdpi.png', 'avatar_f3_hdpi.png', 'avatar_f4_hdpi.png'];
+
         return [
             'name' => fake()->name(),
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'total_exp' => fake()->numberBetween(0, 10000),
-            'image_url' => 'https://i.pravatar.cc/150?u=' . fake()->uuid,
+            'image_url' => 'https://res.cloudinary.com/' . env('CLOUDINARY_CLOUD_NAME') . '/image/upload/' . env('CLOUDINARY_VERSION') . '/snappie/assets/avatar/' . fake()->randomElement($userAvatarOptions),
             'additional_info' => [
                 'user_detail' => [
                     'gender' => fake()->randomElement(['male', 'female', 'other']),
